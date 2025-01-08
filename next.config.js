161646abff4527045a7e1f0c1ca303f682115585
sametsahinnet <sametsahin.php@gmail.com>
2025-01-08 20:34:57 +0300

    try that

--- next.config.js
+++ next.config.js
@@ -4,6 +4,57 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Optimize chunk size
+  webpack: (config, { isServer }) => {
+    // Optimize client-side chunks
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        chunks: "all",
+        minSize: 20000,
+        maxSize: 24000,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          framework: {
+            name: "framework",
+            chunks: "all",
+            test: /[\\/]node_modules[\\/]/,
+            priority: 40,
+            enforce: true,
+          },
+          lib: {
+            test(module) {
+              return module.size() > 160000;
+            },
+            name(module) {
+              return `lib-${module.libIdent({ context: __dirname })}`;
+            },
+            priority: 30,
+            minChunks: 1,
+            reuseExistingChunk: true,
+          },
+          commons: {
+            name: "commons",
+            chunks: "all",
+            minChunks: 2,
+            priority: 20,
+          },
+          shared: {
+            name(module, chunks) {
+              return `shared-${chunks.map((c) => c.name).join("~")}`;
+            },
+            priority: 10,
+            minChunks: 2,
+            reuseExistingChunk: true,
+          },
+        },
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
