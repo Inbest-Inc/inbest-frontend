cbf530291757ef67abc43b9f5942b00511ca3de5
sametsahinnet <sametsahin.php@gmail.com>
2025-01-08 18:42:30 +0300

    try again

--- next.config.js
+++ next.config.js
@@ -15,37 +15,45 @@ const nextConfig = {
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   swcMinify: true,
-  webpack: (config, { isServer }) => {
-    // Only apply to client-side bundles
-    if (!isServer) {
-      config.optimization.splitChunks = {
+  webpack: (config) => {
+    config.optimization = {
+      ...config.optimization,
+      minimize: true,
+      splitChunks: {
         chunks: "all",
-        maxInitialRequests: 25,
-        minSize: 20000,
-        maxSize: 20 * 1024 * 1024, // 20MB max chunk size
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        maxSize: 15 * 1024 * 1024, // 15MB max chunk size
         cacheGroups: {
-          default: false,
-          vendors: false,
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return `npm.${packageName.replace("@", "")}`;
+            },
+            priority: 20,
+          },
           framework: {
             name: "framework",
-            test: /[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
+            test: /[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|next|@next)/,
             priority: 40,
             enforce: true,
+          },
+          lib: {
+            test: /[\\/]node_modules[\\/](@tremor|framer-motion|chart\.js)[\\/]/,
+            name: "ui-lib",
+            priority: 30,
           },
           commons: {
             name: "commons",
             minChunks: 2,
-            priority: 20,
-          },
-          lib: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: 30,
-            minChunks: 1,
-            reuseExistingChunk: true,
+            priority: 10,
           },
         },
-      };
-    }
+      },
+    };
     return config;
   },
 };
