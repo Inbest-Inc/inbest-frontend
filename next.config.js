a2b366903a1e916ea1a4e93828f91929f1bc0aea
sametsahinnet <sametsahin.php@gmail.com>
2025-01-08 18:40:01 +0300

    try

--- next.config.js
+++ next.config.js
@@ -15,6 +15,39 @@ const nextConfig = {
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    // Only apply to client-side bundles
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        chunks: "all",
+        maxInitialRequests: 25,
+        minSize: 20000,
+        maxSize: 20 * 1024 * 1024, // 20MB max chunk size
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          framework: {
+            name: "framework",
+            test: /[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
+            priority: 40,
+            enforce: true,
+          },
+          commons: {
+            name: "commons",
+            minChunks: 2,
+            priority: 20,
+          },
+          lib: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: 30,
+            minChunks: 1,
+            reuseExistingChunk: true,
+          },
+        },
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
