f43c064168aa2756e5131e4114f59dc1406891ca
sametsahinnet <sametsahin.php@gmail.com>
2025-01-08 21:50:24 +0300

    hmm

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -24,6 +22,9 @@ const nextConfig = {
 };
 
 if (process.env.NODE_ENV === "development") {
+  const { setupDevPlatform } = await import(
+    "@cloudflare/next-on-pages/next-dev"
+  );
   await setupDevPlatform();
 }
 
