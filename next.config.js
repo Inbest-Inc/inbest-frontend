b66131f09f82dc1685eeece951c4627cae31dac6
sametsahinnet <sametsahin.php@gmail.com>
2025-01-08 17:12:25 +0300

    try again

--- next.config.js
+++ next.config.js
@@ -13,6 +13,49 @@ const nextConfig = {
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+  webpack: (config, { isServer }) => {
+    // Optimize bundle size
+    config.optimization.splitChunks = {
+      chunks: "all",
+      maxInitialRequests: 25,
+      minSize: 20000,
+      maxSize: 24000000, // 24MB to stay safely under 25MB limit
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        framework: {
+          chunks: "all",
+          name: "framework",
+          test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
+          priority: 40,
+          enforce: true,
+        },
+        commons: {
+          name: "commons",
+          chunks: "all",
+          minChunks: 2,
+          priority: 20,
+        },
+        lib: {
+          test(module) {
+            return (
+              module.size() > 160000 &&
+              /node_modules[/\\]/.test(module.identifier())
+            );
+          },
+          name(module) {
+            const hash = crypto.createHash("sha1");
+            hash.update(module.identifier());
+            return "lib-" + hash.digest("hex").substring(0, 8);
+          },
+          priority: 30,
+          minChunks: 1,
+          reuseExistingChunk: true,
+        },
+      },
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
