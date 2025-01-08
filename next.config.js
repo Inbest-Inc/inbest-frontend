c5ee368d2b7eefd588d9fa780b7ce1e9745fab3b
sametsahinnet <sametsahin.php@gmail.com>
2025-01-08 18:52:59 +0300

    try

--- next.config.js
+++ next.config.js
@@ -25,16 +25,35 @@ const nextConfig = {
     // Disable webpack caching
     config.cache = false;
 
-    // Set small chunk size
-    config.optimization.splitChunks = {
-      chunks: "all",
-      maxSize: 20000000, // 20MB
+    // Configure chunk splitting
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        chunks: "all",
+        maxInitialRequests: 25,
+        minSize: 20000,
+        maxSize: 20000000, // 20MB
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          pages: {
+            test: /[\\/]pages[\\/]/,
+            name: "pages",
+            chunks: "all",
+            maxSize: 20000000,
+          },
+          commons: {
+            name: "commons",
+            chunks: "all",
+            minChunks: 2,
+            maxSize: 20000000,
+          },
+        },
+      },
+      runtimeChunk: { name: "runtime" },
     };
 
     return config;
-  },
-  env: {
-    NEXT_RUNTIME: "edge",
   },
 };
 
