b47761f2a4b742d488176f42dad2f79c768f0c58
sametsahinnet <sametsahin.php@gmail.com>
2025-01-08 22:05:47 +0300

    try that

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -21,6 +27,10 @@ const nextConfig = {
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": resolve(__dirname, "./src"),
+    };
     return config;
   },
 };
