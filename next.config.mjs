4f595f6f80008045ddbbc2d728ba6d6cd45e20a2
sametsahinnet <sametsahin.php@gmail.com>
2025-01-08 21:30:34 +0300

    try again

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,35 @@ import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
       },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
     ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    formats: [
+      "image/webp",
+      "image/avif",
+      "image/svg+xml",
+      "image/jpeg",
+      "image/png",
+      "image/gif",
+    ],
+    minimumCacheTTL: 60,
   },
-  transpilePackages: ["@tremor/react"],
-  reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    optimizePackageImports: ["@tremor/react"],
+  },
 };
 
 if (process.env.NODE_ENV === "development") {
