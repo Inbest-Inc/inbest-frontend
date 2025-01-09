f43c3418a17d8486bcb61b580a5176c65ef7ced8
sametsahinnet <sametsahin.php@gmail.com>
2025-01-09 14:20:51 +0300

    forgot icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Inbest - Social Investing Platform",
   description: "Connect, share and learn with fellow investors",
+  icons: {
+    icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/icon.png" },
+      { url: "/icon-dark.png" },
+    ],
+    apple: [{ url: "/apple-icon.png" }],
+    shortcut: [{ url: "/favicon.svg" }],
+  },
 };
 
 export default function RootLayout({
