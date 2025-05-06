5a1965821ed0872578754d9a90591875dcfe7ed8
sametsahinnet <sametsahin.php@gmail.com>
2025-05-06 12:17:37 +0300

    close position text

--- src/components/ManageableActivityTable.tsx
+++ src/components/ManageableActivityTable.tsx
@@ -118,12 +118,13 @@ const DeleteConfirmModal = ({
                     as="h3"
                     className="text-base font-semibold leading-6 text-gray-900"
                   >
-                    Delete ${stock.symbol}
+                    CLOSE position: ${stock.symbol}
                   </Dialog.Title>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      Are you sure you want to delete {stock.name} ($
-                      {stock.symbol}) from your portfolio? This action cannot be
+                      Are you sure you want to close your position for{" "}
+                      {stock.name} ($
+                      {stock.symbol}) in your portfolio? This action cannot be
                       undone.
                     </p>
                   </div>
@@ -135,7 +136,7 @@ const DeleteConfirmModal = ({
                   className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 sm:col-start-2"
                   onClick={onConfirm}
                 >
-                  Delete
+                  CLOSE position
                 </button>
                 <button
                   type="button"
